51f8af5eaacf2815096258a076b95be3bd039806
Juan Arana <arana.juanpablo@gmail.com>
2016-10-23 11:07:12 -0300

    Agregados ejemplos

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
 #include "lista_test.h";
-
+#include "persona_adapter.h"
 int main(){
     printf("------Iniciando------\n");
-    run();
+    //run();
+
+    /* TXT TO BIN */
+    FILE * txt = fopen("personas_data_text","r");
+    FILE * bin = fopen("personas_data","wb");
+    char tmp[200];
+    tinfo d;
+
+    if(!txt || !bin){ return 1; }
+
+    while(fgets(tmp,42,txt)){
+        info_load_txt(tmp,&d);
+        info_show(&d,bin,1);
+        info_show(&d,stdout,0);
+    }
+
     printf("--------FIN----------\n");
 }
